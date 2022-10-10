e63ce2c2bf245b8cdff7ae3f7490c5be83343b2f
JBarber21 <jonathon2997@gmail.com>
2022-10-09 20:35:39 -0500

    Creating database
    This is different from what I'm used to as far as SQL goes. For now, this is a good start, and needs to be tested. Not connected to the front end yet. Also testing fork again. Hopefully I have it working now

---
+++ Back End/Database/database.jl
@@ -0,0 +1,44 @@
+
+using SQLite
+
+db = SQLite.DB("Pao-CardData.db")
+
+#user table
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS Users 
+    (userId INTEGER PRIMARY KEY, userName TEXT, password TEXT, email TEXT, age INTEGER,
+    level INTEGER, rank TEXT, userType TEXT, deckOwned OBJECT, cardOwned OBJECT, 
+    previousMatches OBJECT)")
+    101
+
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS Cards
+    (cardId INTEGER PRIMARY KEY, cardName TEXT, CardType TEXT, cardEffect OBJECT)") 
+    101
+
+#Relating users to cards
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS User_Cards
+    (userID INTEGER, cardId INTEGER)") 
+    101    
+
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS Decks
+    (deckID INTEGER PRIMARY KEY, deckName TEXT)") 
+    101
+
+#Relating Users to decks
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS User_Decks
+    (userId INTEGER, deckId INTEGER)") 
+    101
+
+#Relating Decks to cards
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS Deck_Cards
+    (deckId INTEGER, cardId INTEGER)") 
+    101
+
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS Matches 
+    (matchId INTEGER PRIMARY KEY, hostId INTEGER, challengerId INTEGER, winnerName TEXT,
+    loserName TEXT)")
+    101
+
+#Relating Users to Matches
+SQLite.execute(db, "CREATE TABLE IF NOT EXISTS User_Matches
+    (userId INTEGER, matchId INTEGER)")
+    101
