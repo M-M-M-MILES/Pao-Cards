d75257d2cc33f6f67c30dee1e53825c2e7699501
Jake Vinet <89436877+jakeVinet@users.noreply.github.com>
2022-10-19 16:07:05 -0500

    Dejean advice

--- Networking.jl
+++ Networking.jl
@@ -1,9 +1,11 @@
 using Sockets
 
 errormonitor(@async begin
-           server = listen(2000)
-           while true
-               sock = accept(server)
-               println("Hello World\n")
-           end
-       end)
+    server = listen(443) # or 80
+    while true
+        sock = accept(server)
+        @async while isopen(sock)
+            write(sock, readline(sock, keep=true))
+        end
+    end
+end)
--- OtherComp.jl
+++ OtherComp.jl
@@ -1,1 +1,5 @@
-connect(2000)
+clientside = connect(443)#or 80
+errormonitor(@async while isopen(clientside)
+    write(stdout, readline(clientside, keep=true))
+end)
+println(clientside,"Hello World from the Echo Server")
