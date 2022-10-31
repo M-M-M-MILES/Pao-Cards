09f157b7cde452f19fdb4fa44f74c67aa7b281e5
Miles <98557226+M-M-M-MILES@users.noreply.github.com>
2022-10-31 13:55:27 -0500

    Update Player.jl
    updated player.jl

--- PaoCards/Player.jl
+++ PaoCards/Player.jl
@@ -1,8 +1,13 @@
 mutable struct Player
     PlayerId::Int
-    Deck::Deck
-    Health::Int
+    Deck::Deck[]
     Rank::Int
     Currency::Int
     
+end
+mutable struct Dueler
+    PlayerId::Int
+    CurrentDeck::CurrentDeck
+    Rank::Int
+    Health::Int
 end
