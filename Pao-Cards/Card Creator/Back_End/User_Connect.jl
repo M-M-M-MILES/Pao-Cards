1290d680f6e85f65da0d96ba20c719c4399053bb
Jonathon Barber <jonathon2997@gmail.com>
2022-11-02 07:54:49 -0500

    Ignore this

--- Pao-Cards/Card Creator/Back_End/User_Connect.jl
+++ Pao-Cards/Card Creator/Back_End/User_Connect.jl
@@ -5,35 +5,41 @@ using("Models/Card.jl")
 #import .Card_mod
 
 function getUser(x, y)
-    currentUser = SQLite.execute(db, "SELECT * FROM Users WHERE userId = ('$x')")
-    user = SQLite.execute(db, "SELECT * FROM Users WHERE userId = ('$y')")
 
-    if(currentUser.userId = user.userId || currentUser.userRole = "Admin")
-        return user
-    else
-        return user.userName + user.level + user.rank
-    end
 end
 
 function createUser(userName, password, email, dateOfBirth)
-    #need to add troubleshooting (checking if a user already exists, if passwords/emails match, etc)
-    SQLite.execute(db, "INSTER INTO Users (userName) VALUES ('$userName')")
-    SQLite.execute(db, "INSTER INTO Users (password) VALUES ('$password')")
-    SQLite.execute(db, "INSTER INTO Users (email) VALUES ('$email')")
-    SQLite.execute(db, "INSTER INTO Users (dateOfBirth) VALUES ('$dateOfBirth')")
-    SQLite.execute(db, "INSTER INTO Users (userRole) VALUES ('Player')")
-    SQLite.execute(db, "INSTER INTO Users (level) VALUES ('1')")
-    SQLite.execute(db, "INSTER INTO Users (rank) VALUES ('No Rank')")
+    
+    #Need to check to see if username already exists
+    try
+        SQLite.execute(db, "INSERT INTO Users (userName) VALUES ('$userName')")
+
+        catch
+            println("Unable to create user")
+    end
+
+    try
+        SQLite.execute(db, "UPDATE Users SET email = '$email' WHERE userName = '$userName'")
+        catch
+            println("Error when adding email")
+    end
+
+    try
+        SQLite.execute(db, "UPDATE Users SET email = '$email' WHERE userName = '$userName'")
+        catch
+            println("Error when adding card type")
+    end
+
 end
 
 function createAdminUser(userName, password, email, dateOfBirth) 
-    #need to add troubleshooting (checking if a user already exists, if passwords/emails match, etc)
-    #in the future, this will change, but this should work for now
-    SQLite.execute(db, "INSTER INTO Users (userName) VALUES ('$userName')")
-    SQLite.execute(db, "INSTER INTO Users (password) VALUES ('$password')")
-    SQLite.execute(db, "INSTER INTO Users (email) VALUES ('$email')")
-    SQLite.execute(db, "INSTER INTO Users (dateOfBirth) VALUES ('$dateOfBirth')")
-    SQLite.execute(db, "INSTER INTO Users (userRole) VALUES ('Admin')")
-    SQLite.execute(db, "INSTER INTO Users (level) VALUES ('1')")
-    SQLite.execute(db, "INSTER INTO Users (rank) VALUES ('No Rank')")
+
+end
+
+function login(userName, password)
+
+end
+
+function logout()
+
 end
